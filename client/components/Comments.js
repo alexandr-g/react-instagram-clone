52ebb691207897f96802115d45f2be1a53c98322
Alexander Grischuk <alexgrischuk@gmail.com>
2016-11-14 00:11:27 +0100

    Add comments component

---
+++ client/components/Comments.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Comments = React.createClass({
+    render() {
+        <div className='comment'>
+            I am the comments
+        </div>
+    }
+})
+
+export default Comments
